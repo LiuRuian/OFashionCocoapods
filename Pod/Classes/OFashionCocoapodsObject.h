4db2053e807d33fcf0f044ec63f04655deaef8bc
ruian.liu <ruian.liu@mfashion.com.cn>
2015-10-12 16:32:20 +0800

    add ObjectClass

---
+++ Pod/Classes/OFashionCocoapodsObject.h
@@ -0,0 +1,13 @@
+//
+//  OFashionCocoapodsObject.h
+//  NSPredicate_Code
+//
+//  Created by Ryan on 15/10/12.
+//  Copyright © 2015年 Ryan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OFashionCocoapodsObject : NSObject
+
+@end
---
+++ Pod/Classes/OFashionCocoapodsObject.m
@@ -0,0 +1,13 @@
+//
+//  OFashionCocoapodsObject.m
+//  NSPredicate_Code
+//
+//  Created by Ryan on 15/10/12.
+//  Copyright © 2015年 Ryan. All rights reserved.
+//
+
+#import "OFashionCocoapodsObject.h"
+
+@implementation OFashionCocoapodsObject
+
+@end
